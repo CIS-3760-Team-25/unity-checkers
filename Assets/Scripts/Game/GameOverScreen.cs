bf5125d9fec1c83df988c4a2db56fc825af7ffcc
Pasan <pasanu@protonmail.com>
2021-10-24 11:29:43 -0400

    Added Game Over Screen

---
+++ Assets/Scripts/Game/GameOverScreen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameOverScreen : MonoBehaviour
+{
+  public Text winnerTxt;
+  public void Setup(int winner)
+  {
+    gameObject.SetActive(true);
+
+    if (winner == 1)
+    {
+      winnerTxt.text = "Black Won!";
+    }
+    else if (winner == -1)
+    {
+      winnerTxt.text = "White Won!";
+    }
+    else
+    {
+      winnerTxt.text = "Tie";
+    }
+  }
+   
+  public void MainMenuBtn()
+  {
+    SceneManager.LoadScene("MenuScene");
+  }
+
+  public void RestartButton()
+  {
+      SceneManager.LoadScene("GameScene");
+  }
+
+}
+ 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,10 +8,14 @@ public class GameController : MonoBehaviour
   // public Player activePlayer;
 
   public Board board;
-
+  public GameOverScreen gameOverScreen;
   // private EventSystem menuEventSystem;
   // private EventSystem gameEventSystem;
 
+  public void triggerGameOverScreen(int winner)
+  {
+    gameOverScreen.Setup(winner);
+  }
   void Awake()
   {
 
@@ -36,4 +40,11 @@ public class GameController : MonoBehaviour
   {
 
   }
+  void Update()
+  {
+    //Temporarily triggers for game over
+    if (Input.GetKeyDown("z")) triggerGameOverScreen(1);
+    if (Input.GetKeyDown("x")) triggerGameOverScreen(-1);
+  }
+
 }
