0a90d8b34a849ec6653b7b45ca859201aceb8994
ajferron <aferron@uoguelph.ca>
2021-11-13 22:32:42 -0500

    Fix indentation

--- Assets/Tests/EditModeTests/EditTests.cs
+++ Assets/Tests/EditModeTests/EditTests.cs
@@ -6,10 +6,10 @@ using UnityEngine.TestTools;
 
 public class EditTests
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void EditTestsSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
+  // A Test behaves as an ordinary method
+  [Test]
+  public void EditTestsSimplePasses()
+  {
+    // Use the Assert class to test conditions
+  }
 }
